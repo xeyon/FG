e35c24af996cf9295235dd3b76cb2a39e1d549e9
Lars Toenning <dev@ltoenning.de>
2022-03-27 12:52:57 +0200

    Update swift license headers

--- src/Network/Swift/swift_connection.hxx
+++ src/Network/Swift/swift_connection.hxx
@@ -1,22 +1,8 @@
-// swift_connection.hxx
-//
-// Copyright (C) 2019 - swift Project Community / Contributors (http://swift-project.org/)
-// Adapted to Flightgear by Lars Toenning <dev@ltoenning.de>
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+/*
+ * SPDX-FileCopyrightText: (C) 2019-2022 swift Project Community / Contributors (https://swift-project.org/)
+ * SPDX-FileCopyrightText: (C) 2019-2022 Lars Toenning <dev@ltoenning.de>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #pragma once
 
 #include <memory>
