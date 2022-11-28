82dbef0c12208a1406c3b665d0d4b257f5d71aa0
James Turner <james@flightgear.org>
2022-11-28 15:12:44 +0000

    Make Swift tests conditional on Swift support

--- test_suite/unit_tests/Network/TestSuite.cxx
+++ test_suite/unit_tests/Network/TestSuite.cxx
@@ -3,9 +3,15 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "config.h"
+
+#if defined(ENABLE_SWIFT)
+
 #include "test_swiftAircraftManager.hxx"
 #include "test_swiftService.hxx"
 
 // Set up the unit tests.
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(SwiftAircraftManagerTest, "Unit tests");
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(SwiftServiceTest, "Unit tests");
+
+#endif
