6e2bf894166c333f96e6c6bd68daedd1a1de14a7
xeyon <xeyon@163.com>
2023-05-15 12:32:57 +0800

    add dll functions to look up rwy

---
+++ src/Airports/apt_loader_wrapper.cpp
@@ -0,0 +1,598 @@
+#include "apt_loader_wrapper.h"
+#include "Navaids/NavDataCache.hxx"
+#include "airport.hxx"
+#include "runways.hxx"
+#include "groundnetwork.hxx"
+#include <Main/options.hxx>
+//#include <Main/fg_props.hxx>
+#include <Main/fg_init.hxx>
+#include <simgear/props/props_io.hxx>
+#include <simgear/structure/exception.hxx>
+
+flightgear::NavDataCache* cache;
+
+int initGlobals(char* sDatapath)
+{
+    if (!globals) {
+        globals = new FGGlobals;
+
+        globals->set_fg_home(SGPath(".\\"));
+        if (sDatapath) {
+            globals->set_fg_root(SGPath(sDatapath));
+        } else {
+            globals->set_fg_root(SGPath("E:\\FlightGearBuild\\fgdata"));
+        }
+    } else {
+        return EXIT_SUCCESS;
+    }
+
+    cout << "Data home folder:" << globals->get_fg_home();
+
+    // Load the configuration parameters.  (Command line options
+    // override config file options.  Config file options override
+    // defaults.)
+    int configResult = fgInitConfig(0, nullptr, true);
+    if (configResult == flightgear::FG_OPTIONS_ERROR) {
+        return EXIT_FAILURE;
+    } else if (configResult == flightgear::FG_OPTIONS_EXIT) {
+        return EXIT_SUCCESS;
+    }
+}
+
+int loadPropsXml(const char* path)
+{
+    SGPath fullpath = SGPath::fromLocal8Bit(path);
+
+    try {
+        readProperties(fullpath, globals->get_props());
+    } catch (const sg_exception& e) {
+        cout << "Error reading properties: ", e;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int savePropsXml(const char* path)
+{
+    SGPath autosaveFile = SGPath::fromLocal8Bit(path);
+
+    autosaveFile.create_dir(0700);
+    SG_LOG(SG_IO, SG_INFO, "Saving user settings to " << autosaveFile);
+    try {
+        writeProperties(autosaveFile, globals->get_props(), false, SGPropertyNode::USERARCHIVE);
+    } catch (const sg_exception& e) {
+        cout << "Error writing props to xml:", e;
+        return EXIT_FAILURE;
+    }
+    SG_LOG(SG_INPUT, SG_DEBUG, "Finished Saving user settings");
+
+    return EXIT_SUCCESS;
+}
+
+int getPropsBool(const char* name, int* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        *val = prop->getBoolValue();
+        return 1;
+    }
+}
+
+int getPropsInt(const char* name, int* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        *val = prop->getIntValue();
+        return 1;
+    }
+
+}
+int getPropsLong(const char* name, long long* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        *val = prop->getLongValue();
+        return 1;
+    }
+}
+int getPropsFloat(const char* name, float* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        *val = prop->getFloatValue();
+        return 1;
+    }
+}
+int getPropsDouble(const char* name, double* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        *val = prop->getDoubleValue();
+        return 1;
+    }
+}
+int getPropsString(const char* name, const char* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, false);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        val = prop->getStringValue().c_str();
+        return 1;
+    }
+}
+
+int getPropsBoolList(const char* name, int* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, false);
+
+        if (prop == NULL)
+            return i;
+        else {
+            *val = prop->getBoolValue();
+        }
+        val++;
+    }
+    return num;
+}
+int getPropsIntList(const char* name, int* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, false);
+
+        if (prop == NULL)
+            return i;
+        else {
+            *val = prop->getIntValue();
+        }
+        val++;
+    }
+    return num;
+}
+int getPropsLongList(const char* name, long long* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, false);
+
+        if (prop == NULL)
+            return i;
+        else {
+            *val = prop->getLongValue();
+        }
+        val++;
+    }
+    return num;
+}
+int getPropsFloatList(const char* name, float* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, false);
+
+        if (prop == NULL)
+            return i;
+        else {
+            *val = prop->getFloatValue();
+        }
+        val++;
+    }
+    return num;
+}
+int getPropsDoubleList(const char* name, double* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, false);
+
+        if (prop == NULL)
+            return i;
+        else {
+            *val = prop->getDoubleValue();
+        }
+        val++;
+    }
+    return num;
+}
+
+int setPropsBool(const char* name, int val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setBoolValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsInt(const char* name, int val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setIntValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsLong(const char* name, long long val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setLongValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsFloat(const char* name, float val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setFloatValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsDouble(const char* name, double val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setDoubleValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsString(const char* name, const char* val)
+{
+    SGPropertyNode* prop = globals->get_props()->getNode(name, true);
+
+    if (prop == NULL)
+        return 0;
+    else {
+        prop->setStringValue(val);
+        prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+        return 1;
+    }
+}
+int setPropsBoolList(const char* name, int* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+        
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, true);
+
+        if (prop == NULL)
+            return i;
+        else {
+            prop->setBoolValue(*val);
+            prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+            val++;
+        }
+    }
+    return num;
+}
+int setPropsIntList(const char* name, int* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, true);
+
+        if (prop == NULL)
+            return i;
+        else {
+            prop->setIntValue(*val);
+            prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+            val++;
+        }
+    }
+    return num;
+}
+int setPropsLongList(const char* name, long long* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, true);
+
+        if (prop == NULL)
+            return i;
+        else {
+            prop->setLongValue(*val);
+            prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+            val++;
+        }
+    }
+    return num;
+}
+int setPropsFloatList(const char* name, float* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, true);
+
+        if (prop == NULL)
+            return i;
+        else {
+            prop->setFloatValue(*val);
+            prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+            val++;
+        }
+    }
+    return num;
+}
+int setPropsDoubleList(const char* name, double* val, int num)
+{
+    SGPropertyNode* prop = NULL;
+
+    for (int i = 0; i < num; i++) {
+        prop = globals->get_props()->getNode(name, i, true);
+
+        if (prop == NULL)
+            return i;
+        else {
+            prop->setDoubleValue(*val);
+            prop->setAttribute(SGPropertyNode::USERARCHIVE, true);
+            val++;
+        }
+    }
+    return num;
+}
+
+int initNavCache(char* sDatapath)
+{
+    if (!globals) {
+        initGlobals(sDatapath);
+    }
+
+    if (flightgear::NavDataCache::isAnotherProcessRebuilding()) {
+
+        cout << "Another process is rebuilding NavCache. Quit!\n";
+        return -1;
+    }
+
+    cache = (flightgear::NavDataCache::createInstance());
+
+    if (cache->isRebuildRequired()) {
+        auto phase = cache->rebuild();
+        //cout << "please wait for the rebuild patiently...\n";
+        return phase;
+    }
+    //cout << "NavDataCache rebuild done!\n";
+    return flightgear::NavDataCache::REBUILD_DONE;
+}
+
+int getRebuildProgress()
+{
+    auto phase = cache->rebuild();
+    return (phase * 100) + cache->rebuildPhaseCompletionPercentage();
+}
+
+int getAirportGates(const char* sICAO, char* result[])
+{
+    if (!cache) {
+        initNavCache(NULL);
+    }
+
+    const string airportId = sICAO;
+    const FGAirport* apt = fgFindAirportID(airportId);
+    if (apt) {
+        int nRw;
+        int numMatches = 0;
+        auto gateList = apt->groundNetwork()->allParkings();
+        nRw = gateList.size();
+        //char** result = (char**)malloc(sizeof(char*) * (nRw));
+        for (auto rwId : gateList) {
+            result[numMatches++] = (char*)rwId->ident().c_str();
+            //cout << "\tRW id: " << rwId->ident().c_str();
+        }
+
+        return numMatches;
+    } else {
+        free(result);
+        result = NULL;
+        return 0;
+    }
+}
+
+int getAirportRunways(const char* sICAO, char* result[])
+{
+    if (!cache) {
+        initNavCache(NULL);
+    }
+
+    const string airportId = sICAO;
+    const FGAirport* apt = fgFindAirportID(airportId);
+    if (apt) {
+        int nRw;
+        int numMatches = 0;
+        auto rwyList = apt->getRunwaysWithoutReciprocals();     //getRunways();
+        nRw = rwyList.size();
+        //result = (char**)malloc(sizeof(char*) * (nRw));
+        for (auto rwId : rwyList) {
+            result[numMatches++] = (char *)rwId->ident().c_str();
+            //cout << "\tRW id: " << rwId->ident().c_str();
+        }
+
+        return numMatches;
+    } else {
+        //free(result);
+        result = NULL;
+        return 0;
+    }
+
+}
+
+int getAirportInfoByICAO(struct Apt_Info *resp)
+{
+    int rwy_cnt, gate_cnt;
+    char icao[5] = "XXXX";
+    memcpy(icao, &(resp->airport_info_icao), 4);
+    string airportId = icao;
+    const FGAirport* apt = fgFindAirportID(airportId);
+
+    if (!apt) return 0;
+
+    // list of gates
+    auto gateList = apt->groundNetwork()->allParkings();
+    gate_cnt = gateList.size();
+
+    int i = 0;
+    resp->db_info[i++] = gate_cnt * 4;  // i = 0;
+    for (auto rwId : gateList) {
+        strcpy_s((char *)&(resp->db_info[i++]), 4, (char*)rwId->ident().c_str());  // i = 1 to gate_cnt
+    }
+
+    // list of runways
+    auto rwyList = apt->getRunwaysWithoutReciprocals();
+    rwy_cnt = rwyList.size();
+    resp->db_info[i++] = rwy_cnt * 4; // i = (gate_cnt +1);
+    resp->db_info[i + rwy_cnt] = rwy_cnt * 4; // i = (gate_cnt + rwy_cnt+ 2);
+
+    for (auto rwId : rwyList) {
+        // i = (gate_cnt + 2) to (gate_cnt + rwy_cnt + 1)
+        strcpy_s((char*)&(resp->db_info[i]), 4, (char*)rwId->ident().c_str());
+        // i = (gate_cnt + rwy_cnt + 3) to (gate_cnt + 2 * rwy_cnt + 2)
+        strcpy_s((char*)&(resp->db_info[i + 1 + rwy_cnt]), 4, (char*)rwId.get()->reciprocalRunway()->ident().c_str());
+        i++;
+    }
+    resp->airport_info_gate_cnt = gate_cnt;
+    resp->airport_info_rwy_cnt = rwy_cnt;
+
+    return i;
+}
+
+int getRunwayGeo(const char* sICAO, const char* sRW[], double* fLat, double* fLon, double* fEle, double* fHdg, int* iWid)
+    {
+    if (!cache) {
+        initNavCache(NULL);
+    }
+    
+    const string airportId = sICAO;
+    const string rwId = sRW[0];
+    const FGAirport* apt = fgFindAirportID(airportId);
+    FGRunwayRef runway = apt->getRunwayByIdent(rwId);
+    
+    if (!runway) return 0;
+
+    fLat[0] = runway.get()->geod().getLatitudeDeg();
+    fLon[0] = runway.get()->geod().getLongitudeDeg();
+    fEle[0] = apt->elevationM();
+    fHdg[0] = runway.get()->headingDeg();
+    *iWid = (int)runway.get()->widthM();
+
+    // Get reciprocal runway
+    runway = runway.get()->reciprocalRunway();
+    fLat[1] = runway.get()->geod().getLatitudeDeg();
+    fLon[1] = runway.get()->geod().getLongitudeDeg();
+    fEle[1] = apt->elevationM();
+    fHdg[1] = runway.get()->headingDeg();
+    // Update reciprocal runway id
+    sRW[1] = runway.get()->ident().c_str();
+
+    return 1;
+}
+
+int getOppRunwayGeo(const char* sICAO, const char* sRW, double* fLat, double* fLon, double* fEle, double* fHdg, int* iWid)
+{
+    if (!cache) {
+        initNavCache(NULL);
+    }
+
+    const string airportId = sICAO;
+    const string rwId = sRW;
+    const FGAirport* apt = fgFindAirportID(airportId);
+    FGRunwayRef runway = apt->getRunwayByIdent(rwId);
+
+    if (!runway) return 0;
+
+    // Get reciprocal runway
+    runway = runway.get()->reciprocalRunway();
+    *fLat = runway.get()->geod().getLatitudeDeg();
+    *fLon = runway.get()->geod().getLongitudeDeg();
+    *fEle = apt->elevationM();
+    *fHdg = runway.get()->headingDeg();
+    *iWid = (int)runway.get()->widthM();
+
+    return 1;
+}
+
+int findClosestAirport(char* sICAO, char* sRW, double fLat, double fLon, double fRng)
+{
+    int ret = -1;
+    SGGeod apos = SGGeod::fromDeg(fLon, fLat);
+    FGAirport* apt = FGAirport::findClosest(apos, fRng);
+
+    if (!apt) {
+        return ret;
+    }
+
+    memcpy(sICAO, apt->getId().c_str(), 5);
+    ret = 0;
+
+    FGRunway* _runway = apt->findBestRunwayForPos(apos).get();
+    if (!_runway) {
+        return ret;
+    }
+
+    memcpy(sRW, _runway->ident().c_str(), 4);
+    ret = 1;
+
+    return ret;
+}
+    
+/*
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+*/
---
+++ src/Airports/apt_loader_wrapper.h
@@ -0,0 +1,99 @@
+#ifndef APT_LOADER_DLL_H
+#define APT_LOADER_DLL_H
+
+/// <summary>
+/// This reflect the mem block for EP Airport Information Response from Word 11. 
+/// </summary>
+struct Apt_Info {
+
+    int airport_info_icao; /*  0-31 */
+
+    int airport_info_gate_cnt; /*  0-31 */
+
+    int airport_info_rwy_cnt; /*  0-31 */
+
+    /// <summary>
+    /// db_info[0]: GATE ID ARRAY SIZE
+    /// db_info[1 : gate_cnt]: GATE ID ARRAY
+    /// db_info[1+gate_cnt]: END A RUNWAY ID ARRAY SIZE
+    /// db_info[(2+gate_cnt) : (1+GATE_CNT+RWY_CNT)]: END A RWY ID ARRAY
+    /// db_info[2+gate_cnt+rwy_cnt]: END B RUNWAY ID ARRAY SIZE
+    /// db_info[(3+gate_cnt+rwy_cnt) : (2+gate_cnt+2*rwy_cnt)]: END B RWY ID ARRAY
+    /// db_info[3+gate_cnt+2*rwy_cnt]: RUNWAY LIGHT GROUP ARRAY SIZE
+    /// db_info[(4+gate_cnt+2*rwy_cnt) : (3+gate_cnt+3*rwy_cnt)]
+    /// </summary>]
+    int db_info[54];                /* 20 gates + 10 rwy (end a + end b + light)+ 4 for array size */
+} ;
+
+#define EXPORT_DLL __declspec(dllexport)
+
+extern "C" EXPORT_DLL int initGlobals(char* sDatapath);
+
+extern "C" EXPORT_DLL int loadPropsXml(const char* path);
+extern "C" EXPORT_DLL int savePropsXml(const char* path);
+
+extern "C" EXPORT_DLL int getPropsBool(const char* name, int* val);
+extern "C" EXPORT_DLL int getPropsInt(const char* name, int* val);
+extern "C" EXPORT_DLL int getPropsLong(const char* name, long long* val);
+extern "C" EXPORT_DLL int getPropsFloat(const char* name, float* val);
+extern "C" EXPORT_DLL int getPropsDouble(const char* name, double* val);
+extern "C" EXPORT_DLL int getPropsString(const char* name, const char* val);
+extern "C" EXPORT_DLL int getPropsBoolList(const char* name, int* val, int num);
+extern "C" EXPORT_DLL int getPropsIntList(const char* name, int* val, int num);
+extern "C" EXPORT_DLL int getPropsLongList(const char* name, long long* val, int num);
+extern "C" EXPORT_DLL int getPropsFloatList(const char* name, float* val, int num);
+extern "C" EXPORT_DLL int getPropsDoubleList(const char* name, double* val, int num);
+
+extern "C" EXPORT_DLL int setPropsBool(const char* name, int val);
+extern "C" EXPORT_DLL int setPropsInt(const char* name, int val);
+extern "C" EXPORT_DLL int setPropsLong(const char* name, long long val);
+extern "C" EXPORT_DLL int setPropsFloat(const char* name, float val);
+extern "C" EXPORT_DLL int setPropsDouble(const char* name, double val);
+extern "C" EXPORT_DLL int setPropsString(const char* name, const char* val);
+extern "C" EXPORT_DLL int setPropsBoolList(const char* name, int* val, int num);
+extern "C" EXPORT_DLL int setPropsIntList(const char* name, int* val, int num);
+extern "C" EXPORT_DLL int setPropsLongList(const char* name, long long* val, int num);
+extern "C" EXPORT_DLL int setPropsFloatList(const char* name, float* val, int num);
+extern "C" EXPORT_DLL int setPropsDoubleList(const char* name, double* val, int num);
+
+extern "C" EXPORT_DLL int initNavCache(char* sDatapath);
+extern "C" EXPORT_DLL int getRebuildProgress();
+/// <summary>
+///  Retrieve all runways at the airport, but excluding the reciprocal runways.
+/// </summary>
+/// <param name="sICAO"></param>
+/// <param name="nRw"></param>
+/// <returns></returns>
+extern "C" EXPORT_DLL int getAirportRunways(const char* sICAO, char* result[]);
+extern "C" EXPORT_DLL int getAirportGates(const char* sICAO, char* result[]);
+/// <summary>
+/// Retrieve all runways and gates at the airport
+/// </summary>
+/// <param name=""></param>
+/// <returns>valid lenght of db_info</returns>
+extern "C" EXPORT_DLL int getAirportInfoByICAO(struct Apt_Info*);
+/// <summary>
+/// get rwy dimentions
+/// </summary>
+/// <param name="sICAO">input airport icao</param>
+/// <param name="sRW">2 string elements, sRW[0] is the input near end, sRW[1] is output opp end</param>
+/// <param name="fLat">array of 2 Latitudes</param>
+/// <param name="fLon">array of 2 Longtitudes</param>
+/// <param name="fEle">array of 2 Elevations, usually the same</param>
+/// <param name="fHdg">array of 2 Headings, 180deg different</param>
+/// <param name="iWid">runway width</param>
+/// <returns></returns>
+extern "C" EXPORT_DLL int getRunwayGeo(const char* sICAO, const char* rwList[], double* fLat, double* fLon, double* fEle, double* fHdg, int* iWid);
+extern "C" EXPORT_DLL int getOppRunwayGeo(const char* sICAO, const char* sRW, double* fLat, double* fLon, double* fEle, double* fHdg, int* iWid);
+
+/// <summary>
+/// find the closest airport to specific Lat/Lon within range
+/// </summary>
+/// <param name="sICAO"></param>
+/// <param name="fLat"></param>
+/// <param name="fLon"></param>
+/// <param name="fRng"></param>
+/// <returns>0 - if no airport is found</returns>
+extern "C" EXPORT_DLL int findClosestAirport(char* sICAO, char* sRW, double fLat, double fLon, double fRng);
+
+#endif //APT_LOADER_DLL_H
